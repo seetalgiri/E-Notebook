828d5753ab24ce9935770151677f883b314a17c6
seetalgiri <giriseetal@gmail.com>
2023-03-28 08:46:16 +0545

    updated code

---
+++ demo.php
@@ -0,0 +1,3 @@
+<?php
+    echo "gaurab is a girl";
+?>
---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+const togglePassword = document.querySelector('#toggle-password');
+const passwordInput = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function() {
+  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordInput.setAttribute('type', type);
+  
+  this.classList.toggle('hide-password');
+});
