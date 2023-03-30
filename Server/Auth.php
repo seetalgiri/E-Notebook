bd1231d0628c5119f005dd0b2f508167cc457d48
gaurab-hacked <gaurav.sunar@iotech.com.np>
2023-03-30 21:53:12 +0545

    gauab

--- Server/Auth.php
+++ Server/Auth.php
@@ -1,11 +1,24 @@
 <?php
+// Connecting to db
 include "./DBConnect.php";
-if(isset($_POST['login'])){
-    echo "Login me";
-}
 
 // when register
 if(isset($_POST['register'])){
-    echo "Register me";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO `auth` (`name`, `email`, `password`) VALUES ('$name', '$email', '$password')";
+    if(mysqli_query($con, $sql)){
+        echo "inserted success";
+    }
+    else{
+        echo "Cannot insert";
+    }
 }
-?>
+
+// login user authontication
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+}
