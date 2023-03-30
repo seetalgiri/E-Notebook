f3ac3ef47e181137d5c5acd095f37c3f39c2a4b5
gaurab-hacked <gaurav.sunar@iotech.com.np>
2023-03-30 17:40:22 +0545

    gaurab

---
+++ Client/Common/Navigation.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+    <div class="logo">
+        <img src="./Client/images/notebookLogo.png" alt="Logo" />
+    </div>
+    <ul class="nav-list">
+        <li><a href="/">Home</a></li>
+        <div class="line"></div>
+        <li><a href="./Client/pages/notes.html">Notes</a></li>
+        <li><a href="./previousquestions">Previous Question</a></li>
+        <li><a href="./notices">Notices</a></li>
+        <li><a href="./syllabus">Syllabus</a></li>
+    </ul>
+    <div class="rightNav">
+        <div class="login">
+            <a href="./Client/auth/login.html">Login</a>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="./Client/styles/style.css" />
+</head>
+
+<body>
+  <?php include './Client/Common/Navigation.php'; ?>
+
+  <section class="home">
+    <div class="heading">
+      <div class="profile">
+        <img src="./Client/images/profile.png" alt="" />
+      </div>
+      <div class="name">
+        <p class="yname">Your name</p>
+        <p class="email">your email</p>
+      </div>
+    </div>
+    <div class="content">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et qui
+        repudiandae voluptatem, laborum molestiae aliquam sunt odit ab
+        architecto ea non tempore! Iure aliquam error illum enim asperiores
+        qui quae, modi autem consequuntur tenetur harum, repellendus quidem
+        dolores obcaecati sunt porro maiores. Deleniti unde commodi accusamus,
+        facere ipsam quod labore modi quis libero similique ex cum earum porro
+        sunt eos enim numquam dolorem voluptatum illum sequi? Nam tempora
+        porro nesciunt natus eius itaque, eos, soluta ipsa ab, architecto
+        veniam rem fugiat autem reiciendis quod id voluptatum. Eos enim illo
+        aut? Repellendus ipsam beatae ipsum vitae modi veniam rem possimus
+        quaerat.
+      </p>
+      <img src="" alt="" />
+    </div>
+  </section>
+</body>
+<script src="./Client/logic/script.js"></script>
+
+</html>
