40ab0822603fbf112e25a6369371c545f4a1dbd0
seetalgiri <giriseetal@gmail.com>
2023-04-29 20:45:43 +0545

    updated code

--- Client/auth/login.php
+++ Client/auth/login.php
@@ -1,15 +1,13 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "");
+$con = mysqli_connect("localhost", "root", "");
 if (!$con) {
   die("Database connection failed");
 }
-
 // check btn is clicked or not for connect
 
 if (isset($_POST['login'])) {
   $email = $_POST['email'];
   $password = $_POST['password'];
-
   // fetch data from db and check if password and email are correct
   $sql = "SELECT * FROM `auth` WHERE `email` = '$email'";
   $rowEmail = mysqli_query($con, $sql);
@@ -49,14 +47,14 @@ if (isset($_POST['login'])) {
           <h3 id="login">Login</h3>
           <div class="input-box">
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" />
+            <input type="email" id="email" name="email" placeholder="Enter your email" required />
             <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM19.6 8.25L12.53 12.67C12.21 12.87 11.79 12.87 11.47 12.67L4.4 8.25C4.29973 8.19371 4.21192 8.11766 4.14189 8.02645C4.07186 7.93525 4.02106 7.83078 3.99258 7.71937C3.96409 7.60796 3.9585 7.49194 3.97616 7.37831C3.99381 7.26468 4.03434 7.15581 4.09528 7.0583C4.15623 6.96079 4.23632 6.87666 4.33073 6.811C4.42513 6.74533 4.53187 6.69951 4.6445 6.6763C4.75712 6.65309 4.87328 6.65297 4.98595 6.67595C5.09863 6.69893 5.20546 6.74453 5.3 6.81L12 11L18.7 6.81C18.7945 6.74453 18.9014 6.69893 19.014 6.67595C19.1267 6.65297 19.2429 6.65309 19.3555 6.6763C19.4681 6.69951 19.5749 6.74533 19.6693 6.811C19.7637 6.87666 19.8438 6.96079 19.9047 7.0583C19.9657 7.15581 20.0062 7.26468 20.0238 7.37831C20.0415 7.49194 20.0359 7.60796 20.0074 7.71937C19.9789 7.83078 19.9281 7.93525 19.8581 8.02645C19.7881 8.11766 19.7003 8.19371 19.6 8.25Z" />
             </svg>
           </div>
           <div class="input-box">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" />
+            <input type="password" id="password" name="password" placeholder="Enter your password" required/>
             <div class="password">
               <svg id="showIcon" width="17" height="17" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" onclick="eyeOpen()">
                 <path d="M10.5 8C10.5 8.66304 10.2366 9.29893 9.76777 9.76777C9.29893 10.2366 8.66304 10.5 8 10.5C7.33696 10.5 6.70107 10.2366 6.23223 9.76777C5.76339 9.29893 5.5 8.66304 5.5 8C5.5 7.33696 5.76339 6.70107 6.23223 6.23223C6.70107 5.76339 7.33696 5.5 8 5.5C8.66304 5.5 9.29893 5.76339 9.76777 6.23223C10.2366 6.70107 10.5 7.33696 10.5 8Z" />
