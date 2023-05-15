1da4979018aec95cf93dcadb127a992e740d0fda
gaurab-hacked <gaurabsunar0001@gmail.com>
2023-05-15 12:49:29 +0545

    gaurab

--- admin/notepost.php
+++ admin/notepost.php
@@ -8,7 +8,7 @@
     <title>E-NoteBook Note Post</title>
     <!-- for CSS Style  -->
     <link rel="stylesheet" href="../Client/styles/globalsa.css">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./CSS/faculitya.css">
     <link rel="stylesheet" href="./CSS/notepost.css">
 
