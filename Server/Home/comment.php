e9a1af43d02ead22c5987409fc404df0eb6e2bd0
gaurab-hacked <gaurabsunar0001@gmail.comm>
2023-05-30 16:25:35 +0545

    gaurab

--- Server/Home/comment.php
+++ Server/Home/comment.php
@@ -12,7 +12,6 @@ if (!$con) {
     die("Could not connect to the database");
 }
 
-
 // Check if the request is a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $commentData = json_decode(file_get_contents('php://input'), true);
@@ -27,6 +26,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "INSERT INTO comments (cmt_des, author, postId, date, userid) VALUES ('$comment', '$userName', '$postId', NOW(), '$userId')";
 
     if (mysqli_query($con, $sql)) {
+        // Get the comment ID
+        $commentId = mysqli_insert_id($con);
+
+        // Update the 'comment' column in the 'news' table
+        $sqlSelect = "SELECT comment FROM news WHERE id = $postId";
+        $result = mysqli_query($con, $sqlSelect);
+        $row = mysqli_fetch_assoc($result);
+        $existingComment = $row['comment'];
+
+        if ($existingComment == "") {
+            // If the 'comment' column is empty, directly add the comment ID
+            $updatedComment = $commentId;
+        } else {
+            // If the 'comment' column has existing IDs, concatenate the new ID without a leading comma
+            $updatedComment = $existingComment . ", " . $commentId;
+        }
+
+        $sqlUpdate = "UPDATE news SET comment = '$updatedComment' WHERE id = $postId";
+        mysqli_query($con, $sqlUpdate);
+
         // Comment insertion successful
         $response = array('status' => 'success', 'message' => 'Comment added successfully');
     } else {
