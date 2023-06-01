082d5d8b7a450f4bf10bcc10e7f9a77310c5e8fe
gaurab-hacked <gaurabsunar0001@gmail.comm>
2023-06-01 07:33:10 +0545

    delete added

--- admin/notepost.php
+++ admin/notepost.php
@@ -15,6 +15,38 @@ if (!$con) {
 $sql = "SELECT * FROM `faculty`";
 $resfac = mysqli_query($con, $sql);
 
+
+// Fetching data from the database
+$sqlNote = "SELECT * FROM `notes`";
+$resultNotes = mysqli_query($con, $sqlNote);
+
+if (!$resultNotes) {
+    die("Error fetching data: " . mysqli_error($con));
+}
+
+
+
+// for delete functionality 
+if (isset($_GET['deletenote'])) {
+    // Retrieve the note ID from the URL parameter
+    $noteId = $_GET['deletenote'];
+
+    // Prepare the delete query
+    $deleteQuery = "DELETE FROM notes WHERE id = $noteId";
+
+    // Execute the delete query
+    if (mysqli_query($con, $deleteQuery)) {
+        // Redirect to the same page without the deletenote parameter
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit(); // Make sure to exit after the redirect
+    } else {
+        echo "Error deleting note: " . mysqli_error($con);
+    }
+    // Close the database connection
+    mysqli_close($con);
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,26 +82,36 @@ $resfac = mysqli_query($con, $sql);
                         <th>Note Name</th>
                         <th colspan="2">Action</th>
                     </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>My name is gaurab</td>
-                        <td>BCA</td>
-                        <td>1st sem</td>
-                        <td>Digital Logic</td>
-                        <td>Chapter 1: Introduction this lorem </td>
+                    <?php
+                    // Displaying data rows
+                    while ($row = mysqli_fetch_assoc($resultNotes)) {
+                        $postDes = $row['post_des'] != "" ? $row['post_des'] : "-";
+                        $noteName = $row['note_name'] != "" ? $row['note_name'] : "-";
+                        $noteId = $row['id'];
+
+                        echo "<tr>
+                        <td>{$row['id']}</td>
+                        <td>{$postDes}</td>
+                        <td>{$row['stream_name']}</td>
+                        <td>{$row['sem']}/{$row['year']}</td>
+                        <td>{$row['sub_name']}</td>
+                        <td>{$noteName}</td>
                         <td class='edit twoBtn' id='editbtn'>
-                            <svg id='editbtn' width='17' height='17' viewBox='0 0 25 24' xmlns='http://www.w3.org/2000/svg'>
-                                <path d='M22.5 8.75V7.5L15 0H2.5C1.1125 0 0 1.1125 0 2.5V20C0 21.3875 1.125 22.5 2.5 22.5H10V20.1625L20.4875 9.675C21.0375 9.125 21.7375 8.825 22.5 8.75ZM13.75 1.875L20.625 8.75H13.75V1.875ZM24.8125 13.9875L23.5875 15.2125L21.0375 12.6625L22.2625 11.4375C22.5 11.1875 22.9125 11.1875 23.1625 11.4375L24.8125 13.0875C25.0625 13.3375 25.0625 13.75 24.8125 13.9875ZM20.1625 13.5375L22.7125 16.0875L15.05 23.75H12.5V21.2L20.1625 13.5375Z' />
-                            </svg>
-                        </td>
-                        <td class='delete twoBtn'>
-                            <a name='deletebtn' $row[" id"] . "\">
-                                <svg width='17' height='17' viewBox='0 0 20 23' xmlns='http://www.w3.org/2000/svg'>
-                                    <path d='M6.25 0V1.25H0V3.75H1.25V20C1.25 20.663 1.51339 21.2989 1.98223 21.7678C2.45107 22.2366 3.08696 22.5 3.75 22.5H16.25C16.913 22.5 17.5489 22.2366 18.0178 21.7678C18.4866 21.2989 18.75 20.663 18.75 20V3.75H20V1.25H13.75V0H6.25ZM6.25 6.25H8.75V17.5H6.25V6.25ZM11.25 6.25H13.75V17.5H11.25V6.25Z' />
-                                </svg>
-                            </a>
-                        </td>
-                    </tr>
+            <svg id='editbtn' width='17' height='17' viewBox='0 0 25 24' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M22.5 8.75V7.5L15 0H2.5C1.1125 0 0 1.1125 0 2.5V20C0 21.3875 1.125 22.5 2.5 22.5H10V20.1625L20.4875 9.675C21.0375 9.125 21.7375 8.825 22.5 8.75ZM13.75 1.875L20.625 8.75H13.75V1.875ZM24.8125 13.9875L23.5875 15.2125L21.0375 12.6625L22.2625 11.4375C22.5 11.1875 22.9125 11.1875 23.1625 11.4375L24.8125 13.0875C25.0625 13.3375 25.0625 13.75 24.8125 13.9875Z'/>
+            </svg>
+        </td>
+        <td class='delete twoBtn'>
+        <a href='?deletenote={$noteId}'>
+                <svg width='17' height='17' viewBox='0 0 20 23' xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M6.25 0V1.25H0V3.75H1.25V20C1.25 20.663 1.51339 21.2989 1.98223 21.7678C2.45107 22.2366 3.08696 22.5 3.75 22.5H16.25C16.913 22.5 17.5489 22.2366 18.0178 21.7678C18.4866 21.2989 18.75 20.663 18.75 20V3.75H20V1.25H13.75V0H6.25ZM6.25 6.25H8.75V17.5H6.25V6.25ZM11.25 6.25H13.75V17.5H11.25V6.25Z'/>
+                </svg>
+            </a>
+        </td>
+        </tr>";
+                    }
+                    ?>
+
                 </table>
 
                 <div class="pagination">
