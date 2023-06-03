2c05a5b8811494ec2c3caeda6a381ef0bf536f69
gaurab-hacked <gaurabsunar0001@gmail.comm>
2023-06-03 13:33:34 +0545

    gaurab

--- admin/common/sidenav.php
+++ admin/common/sidenav.php
@@ -20,16 +20,16 @@ if ($privilege > 1) {
 
 include "../Configuration.php";
 //database connection
-$con = mysqli_connect($commonHost, $commonUser, $commonPassword, $commonDbname);
+$conREQPost = mysqli_connect($commonHost, $commonUser, $commonPassword, $commonDbname);
 
-if (!$con) {
+if (!$conREQPost) {
     die("Database connection failed");
 }
 
 // to show all data in frontend
-$sql = "SELECT * FROM `requestpost` WHERE `status` = 0";
-$resfac = mysqli_query($con, $sql);
-$count = mysqli_num_rows($resfac);
+$sqlREQPost = "SELECT * FROM `requestpost` WHERE `status` = 0";
+$resfacREQPost = mysqli_query($conREQPost, $sqlREQPost);
+$countREQPost = mysqli_num_rows($resfacREQPost);
 
 
 
@@ -196,18 +196,20 @@ $count = mysqli_num_rows($resfac);
                 </li>
                 <li class="posrel">
                     <a>
-                        <svg id="notbtn" <?php echo ($count > 0) ? ' onclick="NotbtnClk()"' : '' ?> width="25" height="25" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                        <svg id="notbtn" <?php echo ($countREQPost > 0) ? ' onclick="NotbtnClk()"' : '' ?> width="25" height="25" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
                             <g>
                                 <path d="M12.5 26.25H17.5C17.5 27.625 16.375 28.75 15 28.75C13.625 28.75 12.5 27.625 12.5 26.25ZM26.25 23.75V25H3.75V23.75L6.25 21.25V13.75C6.25 9.875 8.75 6.5 12.5 5.375V5C12.5 3.625 13.625 2.5 15 2.5C16.375 2.5 17.5 3.625 17.5 5V5.375C21.25 6.5 23.75 9.875 23.75 13.75V21.25L26.25 23.75ZM21.25 13.75C21.25 10.25 18.5 7.5 15 7.5C11.5 7.5 8.75 10.25 8.75 13.75V22.5H21.25V13.75Z" />
                                 <circle cx="22" cy="8" r="8" fill="red" />
-                                <text x="22" y="8" text-anchor="middle" alignment-baseline="central" fill="white" font-size="11px"><?php echo $count ?></text>
+                                <text x="22" y="8" text-anchor="middle" alignment-baseline="central" fill="white" font-size="11px">
+                                    <?php echo $countREQPost ?>
+                                </text>
                             </g>
                         </svg>
                     </a>
                     <div id="notrapper">
                         <div id="notification" class="shadow">
                             <div class="contentnotification">
-                                <?php while ($row = mysqli_fetch_array($resfac)) {
+                                <?php while ($row = mysqli_fetch_array($resfacREQPost)) {
                                     echo '
                                 <a href="./requestpost.php?view=' . $row['id'] . '" class="eachContent shadow">
                                     <div class="divsecNotification">
