5adaad00c286336802477fe1df889af51224b6f2
gaurab-hacked <gaurabsunar0001@gmail.comm>
2023-06-05 21:22:51 +0545

    gaurab

--- Server/Home/indexback.php
+++ Server/Home/indexback.php
@@ -14,64 +14,68 @@ if (!$conn) {
 
 // Check if the form is submitted
 if (isset($_POST['postnews']) || isset($_POST['postnewsadmin'])) {
-    $postdes = $_POST['post'];
-    $stream = strtolower($_POST['stream']);
-    $author = $username;
-    $like = "";
-    $comment = "";
+    if (intval($id) > 0) {
+        $postdes = $_POST['post'];
+        $stream = strtolower($_POST['stream']);
+        $author = $username;
+        $like = "";
+        $comment = "";
 
-    // Check if an image is uploaded
-    if (isset($_FILES['image']) && $_FILES['image']['size'] > 0) {
-        $image = $_FILES['image'];
+        // Check if an image is uploaded
+        if (isset($_FILES['image']) && $_FILES['image']['size'] > 0) {
+            $image = $_FILES['image'];
 
-        // Image upload logic
-        $fileName = $image['name'];
-        $fileTmp = $image['tmp_name'];
-        $fileSize = $image['size'];
-        $fileError = $image['error'];
+            // Image upload logic
+            $fileName = $image['name'];
+            $fileTmp = $image['tmp_name'];
+            $fileSize = $image['size'];
+            $fileError = $image['error'];
 
-        // File extension
-        $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+            // File extension
+            $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 
-        // Allowed file extensions
-        $allowedExtensions = ['jpg', 'jpeg', 'png'];
+            // Allowed file extensions
+            $allowedExtensions = ['jpg', 'jpeg', 'png'];
 
-        // Check if the file has a valid extension
-        if (in_array($fileExt, $allowedExtensions)) {
-            // Generate a unique name for the file
-            $newFileName = uniqid('', true) . '.' . $fileExt;
+            // Check if the file has a valid extension
+            if (in_array($fileExt, $allowedExtensions)) {
+                // Generate a unique name for the file
+                $newFileName = uniqid('', true) . '.' . $fileExt;
 
-            // Set the destination path to store the uploaded image
-            $destination = '../uploads/images/' . $newFileName;
+                // Set the destination path to store the uploaded image
+                $destination = '../uploads/images/' . $newFileName;
 
-            // Move the uploaded file to the destination
-            if (move_uploaded_file($fileTmp, $destination)) {
-                // Construct the SQL statement with image data
-                $imagePath =  $uploadFIleFront . 'images/' . $newFileName;
-                $sql = "INSERT INTO `news` (`postdes`, `image`, `stream`, `author`, `post_like`, `comment`) VALUES ('$postdes', '$imagePath', '$stream', '$author', '$like', '$comment')";
+                // Move the uploaded file to the destination
+                if (move_uploaded_file($fileTmp, $destination)) {
+                    // Construct the SQL statement with image data
+                    $imagePath =  $uploadFIleFront . 'images/' . $newFileName;
+                    $sql = "INSERT INTO `news` (`postdes`, `image`, `stream`, `author`, `post_like`, `comment`) VALUES ('$postdes', '$imagePath', '$stream', '$author', '$like', '$comment')";
+                } else {
+                    echo 'Error uploading file.';
+                    exit;
+                }
             } else {
-                echo 'Error uploading file.';
+                echo 'Invalid file extension.';
                 exit;
             }
         } else {
-            echo 'Invalid file extension.';
-            exit;
+            // Construct the SQL statement without image data
+            $sql = "INSERT INTO `news` (`postdes`, `stream`, `author`, `post_like`, `comment`) VALUES ('$postdes', '$stream', '$author', '$like', '$comment')";
         }
-    } else {
-        // Construct the SQL statement without image data
-        $sql = "INSERT INTO `news` (`postdes`, `stream`, `author`, `post_like`, `comment`) VALUES ('$postdes', '$stream', '$author', '$like', '$comment')";
-    }
 
-    // Execute the SQL statement
-    if (mysqli_query($conn, $sql)) {
-        if (isset($_POST['postnewsadmin'])) {
-            header("Location: ../../admin/noticepost.php");
+        // Execute the SQL statement
+        if (mysqli_query($conn, $sql)) {
+            if (isset($_POST['postnewsadmin'])) {
+                header("Location: ../../admin/noticepost.php");
+            } else {
+                header("Location: ../../index.php");
+            }
+            exit;
         } else {
-            header("Location: ../../index.php");
+            echo 'Error: ' . mysqli_error($conn);
         }
-        exit;
     } else {
-        echo 'Error: ' . mysqli_error($conn);
+        header("Location: ../../index.php");
     }
 }
 
