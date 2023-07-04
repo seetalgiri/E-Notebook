e5606bf4361e23fef003f847ca42dd7d3815f281
gaurab-hacked <gaurabsunar0001@gmail.comm>
2023-07-04 12:52:08 +0545

    gaurab

--- admin/noticepost.php
+++ admin/noticepost.php
@@ -8,17 +8,6 @@ if (isset($_GET['error'])) {
         <div class="cross" onclick="crossClk()">X</div>
 
         <div class="innercontent">
-            <!-- <svg
-            width="16"
-            height="16"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            >
-            <path
-                d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
-            />
-            </svg> -->
-
             <svg
             width="16"
             height="16"
